a8d84f4d26fab67bf00edee67373b467d7a180bf
junolee <>
2026-01-29 13:34:28 -0500

    gh assume role policy

--- infra/main.tf
+++ infra/main.tf
@@ -69,7 +69,7 @@ resource "aws_iam_role" "github_actions_role" {
         Condition = {
           StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-          "token.actions.githubusercontent.com:sub" = "repo:junolee/wistia-media-pipeline:*"
+          "token.actions.githubusercontent.com:sub" = "repo:junolee/wistia-media-pipeline:ref:refs/heads/main"
           }
         }
       }
