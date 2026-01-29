13669d69150156957f3706f25765ec877b139c5c
junolee <>
2026-01-29 13:21:49 -0500

    reqs, lambda

--- infra/main.tf
+++ infra/main.tf
@@ -112,3 +112,59 @@ output "github_actions_role_arn" {
   value = aws_iam_role.github_actions_role.arn
 }
 
+#=====================================
+# Lambda
+#=====================================
+
+data "aws_iam_policy_document" "assume_lambda_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "ws_lambda_exec_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda_role.json
+}
+
+locals {
+  permissions = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/SecretsManagerReadWrite",
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  for_each   = toset(local.permissions)
+  policy_arn = each.value
+}
+
+# Lambda function
+
+resource "aws_lambda_function" "ingest" {
+  function_name = "wistia_to_s3"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+
+  s3_bucket = "jl-wistia-pipeline"
+  s3_key    = "lambda/ingest_lambda.zip"
+
+  environment {
+    variables = {
+      YT_MEDIA_ID = "v08dlrgr7v"
+      FB_MEDIA_ID = "gskhw4w4lm"
+      API_TOKEN_SECRET_NAME = "wistia-api-token"
+      BUCKET_NAME = "jl-wistia-pipeline"
+      RAW_PREFIX = "raw"
+      WISTIA_CHECKPOINT_PATH = "state/wistia_checkpoint.json"
+    }
+  }
+}
